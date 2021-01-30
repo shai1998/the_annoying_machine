aab4755aa9a5841ac39ef23fc2514b6ec883e4bf
neil <nilnil47@gmail.com>
2021-01-30 15:16:18 +0200

    add most of the commands

--- main.py
+++ main.py
@@ -33,8 +33,10 @@ class BotController:
     def _add_commands(self):
         self._telegram_updater.dispatcher.add_handler(CommandHandler("start", self._start, pass_args=True))
         self._telegram_updater.dispatcher.add_handler(CommandHandler("stop", self._stop, pass_args=True))
-        self._telegram_updater.dispatcher.add_handler(CommandHandler("setDefaultTime", self._stop, pass_args=True))
-        self._telegram_updater.dispatcher.add_handler(CommandHandler("setDefaultVol", self._stop, pass_args=True))
+        self._telegram_updater.dispatcher.add_handler(
+            CommandHandler("setDefaultTime", self._set_default_vol, pass_args=True))
+        self._telegram_updater.dispatcher.add_handler(
+            CommandHandler("setDefaultVol", self._set_default_time, pass_args=True))
         self._telegram_updater.dispatcher.add_handler(CommandHandler("showConfig", self._print_config, pass_args=True))
         self._telegram_updater.dispatcher.add_handler(CommandHandler("help", self.help, pass_args=True))
 
@@ -42,18 +44,32 @@ class BotController:
         help_string_builder = []
         for cmd in self._telegram_updater.dispatcher.handlers[0]:
             help_string_builder.append(cmd.command[0])
-        print("adsadas", help_string_builder)
         update.message.reply_text("/" + "\n/".join(help_string_builder))
 
+    def _set_volume(self, vol):
+        try:
+            vol = int(vol)
+
+        # todo: print error to the user
+        except ValueError:
+            logging.error("enter number")
+
+        if vol < 0:
+            vol = 0
+        elif vol > 85:
+            vol = 85
+        subprocess.call(f"amixer -q -M sset Headphone {vol}%")
+        return vol
 
     def _set_default_time(self, update, context):
-        if len(context) == 1:
-            self._config["time"] = context[0]
+        if len(context.args) == 1:
+            self._config["time"] = context.args[0]
         self.save_config()
 
     def _set_default_vol(self, update, context):
-        if len(context) == 1:
-            self._config["vol"] = context[0]
+        if len(context.args) == 1:
+            vol = self._set_volume(context.args[0])
+            self._config["vol"] = vol
         self.save_config()
 
     def _print_config(self, update, context):
@@ -63,11 +79,23 @@ class BotController:
         self._telegram_updater.start_polling()
         self._telegram_updater.idle()
 
-    def _stop(self):
+    def _stop(self, update, context):
         self._sound_subprocess.kill()
 
-    def _start(self):
-        self._sound_subprocess = subprocess.Popen("mpg123 /home/pi/12500.mp3", shell=True)
+    def _start(self, update, context):
+        time = self._config["time"]
+        try:
+            time = context.args[0]
+        except KeyError:
+            pass
+
+        try:
+            vol = context.args[1]
+            self._set_volume(vol)
+        except KeyError:
+            pass
+
+        self._sound_subprocess = subprocess.Popen(f"mpg123 /home/pi/12500.mp3 --timeout {time}", shell=True)
 
 
 def main():
@@ -78,4 +106,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # test()
